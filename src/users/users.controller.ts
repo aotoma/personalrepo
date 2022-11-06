44665765e501fc9487bb316a6376bc6fc8155b69
aotoma <tamara@aobyte.com>
2022-11-06 19:15:05 +0400

    added new endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 export const userRouter = express.Router();
@@ -17,5 +17,90 @@ userRouter.post('/users', async (req: Request, res: Response) => {
   });
   res.json(result);
 });
+
+userRouter.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
+
+userRouter.get('/users/:id', async (req: Request, res: Response)=>{
+  const { id } = req.params
+  const users = await prisma.user.findUnique({
+    where: {
+      id: Number(id),
+    },
+  })
+  if(users == null){
+    res.status(404)
+    res.send('User does not exist')
+  }
+  res.json(users)
+});
+
+
+userRouter.put('/users/:id', async (req, res) => {
+  const { id } = req.params
+  const { firstName, lastName, dob, email } = req.body;
+  const dobIsoString = new Date(dob).toISOString();
+  const user = await prisma.user.update({
+    data:{
+      firstName,
+      lastName,
+      dob: dobIsoString,
+      email
+    },
+    where: {
+      id: Number(id),
+    },
+  })
+  res.json(user)
+});
+
+userRouter.delete(`/users/:id`, async (req, res) => {
+  const { id } = req.params
+  const result = await prisma.user.delete({
+    where: {
+      id: Number(id),
+    },
+  })
+  res.json(result)
+});
+
+
+userRouter.get('/users', async (req, res) => {
+  const { searchString }: { searchString?: string } = req.query;
+  const{dobS}: {dobS?: Date} = req.query;
+  const filteredUsers = await prisma.user.findMany({
+    where: {
+      OR: [
+        {
+          firstName: {
+            contains: searchString,
+          },
+        },
+        {
+          lastName: {
+            contains: searchString,
+          },
+        },
+        {
+          email: {
+            contains: searchString
+          }
+        },
+        {
+          dob:{
+          equals: dobS
+          }
+        }
+      ],
+    },
+  })
+  
+  res.json(filteredUsers)
+})
+
+
+
 
 export default { userRouter };
